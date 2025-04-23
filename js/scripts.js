f3d6fef97529e987b4b105686fad0a67a53a5d7d
Tely Lor <tlor1@ewu.edu>
2025-04-23 04:17:52 +0000

    info

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,12 @@
 // Scripting
 
 // Data source
-const url = "https://assets.codepen.io/16425/web-3-spring-2024-roster.json";
+const url = "https://assets.codepen.io/16425/Spring-2025-Roster.json";
 
 // Get data
 fetch(url)
   .then( response  => response.json())
-  .then( data  => {
+  .then( students  => {
     
     // check-check: is the data good?
     console.log(data);
@@ -17,15 +17,26 @@ fetch(url)
     const roster = document.querySelector(".roster");
 
     // loop through data
-    data.forEach( student => {
+    students.forEach( student => {
       
       // template
       const template = `
-          <figure>
-            <figcaption> ${student.Name} </figcaption>
-            <img src=" ${student.Image} " alt=" ${student.Name} ">
-          </figure>
-       `;
+          <li style="border-color:${student.favoriteColor}; 
+     background: linear-gradient(red, ${student.favoriteColor} );
+     
+     ">
+    
+    
+    
+    <img src="${student.imageUrl}"
+        <p>${student.name}
+        ${student.status}
+    </p>
+        
+        <em>${student.favoriteSong}</em>
+        
+    </li>
+    `;
 
       // insert EACH `student` record into container
       roster.insertAdjacentHTML("afterbegin", template);
