63187688c71cf3d33243b55d4510d3ab2b9efb96
Tely Lor <tlor1@ewu.edu>
2025-05-07 15:24:03 +0000

    css, header, border radius

--- js/scripts.js
+++ js/scripts.js
@@ -5,40 +5,39 @@ const url = "https://assets.codepen.io/16425/Spring-2025-Roster.json";
 
 // Get data
 fetch(url)
-  .then( response  => response.json())
-  .then( students  => {
-    
-    // check-check: is the data good?
+  .then(response => response.json())
+  .then(data => {
+	    // check-check: is the data good?
     console.log(data);
     console.log(data.Name);
     console.log(data.Image);
-
-    // get container for data
+	
+      // get container for data
     const roster = document.querySelector(".roster");
 
-    // loop through data
-    students.forEach( student => {
-      
+    // Loop through the data
+    data.forEach(student => {
       // template
       const template = `
-          <li style="border-color:${student.favoriteColor}; 
-     background: linear-gradient(red, ${student.favoriteColor} );
-     
-     ">
-    
-    
-    
-    <img src="${student.imageUrl}"
-        <p>${student.name}
-        ${student.status}
-    </p>
-        
-        <em>${student.favoriteSong}</em>
-        
-    </li>
-    `;
+        <figure class="card" style="border-color: ${student.favoriteColor}">
+          <img src="${student.imageUrl}" alt="${student.name}">
+          <figcaption>
+            <h2 style="color: ${student.favoriteColor}">${student.name}</h2>
+            <p class="motto">${student.motto}</p>
+            <p>Fun Fact: ${student.funFact}</p>
+            <p>Fav Artist/Band: ${student.favoriteBand}</p>
+            
+            <button class="emoji" style="border-color: ${student.favoriteColor}">
+              ${student.status}
+            </button>
+          </figcaption>
+        </figure>
+      `;
 
-      // insert EACH `student` record into container
       roster.insertAdjacentHTML("afterbegin", template);
+
+      // Set the CSS variable
+      const emojiCard = roster.querySelector(".card");
+      emojiCard.style.setProperty('--fav-color', student.favoriteColor);
     });
   });
